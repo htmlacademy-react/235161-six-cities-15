41c7c6c742f8284e33291803887946dcf28de655
Quistis <suicidexhelia@gmail.com>
2024-02-24 13:39:25 +0500

    Внес правки в компонент Gallery

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -2,14 +2,27 @@ type GalleryProps = {
   images: string[];
 }
 
+function GalleryImage({image}: {image: string}): JSX.Element {
+  return (
+    <div
+      key={image}
+      className="offer__image-wrapper"
+    >
+      <img
+        className="offer__image"
+        src={`img/${image}`}
+        alt="Photo studio"
+      />
+    </div>
+  );
+}
+
 function Gallery({images}: GalleryProps): JSX.Element {
+  const galleryImages = images.map((image) => <GalleryImage key={image} image={image}/>);
+
   return (
     <div className="offer__gallery">
-      {images.map((image) => (
-        <div key={image} className="offer__image-wrapper">
-          <img className="offer__image" src={`img/${image}`} alt="Photo studio"/>
-        </div>
-      ))}
+      {galleryImages}
     </div>
   );
 }
