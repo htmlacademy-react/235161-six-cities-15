a7b343f9f4c55a40f07f155ac2a96889197a3838
Quistis <suicidexhelia@gmail.com>
2024-03-04 22:44:12 +0500

    Доработал компоненты ReviewsForm и RatingInput. Добавил обработку смены рейтинга и его добавление в стейт

--- src/components/reviews-form/reviews-form.tsx
+++ src/components/reviews-form/reviews-form.tsx
@@ -2,27 +2,39 @@ import { useState, ChangeEvent } from 'react';
 import { RATINGS } from '../../const';
 import RatingInput from '../../components/rating-input/rating-input';
 
-const MIN_COMMENT_SYMBOLS_COUNT = 50;
+const MIN_COMMENT_LENGTH = 50;
+const MAX_COMMENT_LENGTH = 300;
 
 type ReviewFormData = {
   review: string;
+  rating: number;
 }
 
 function ReviewsForm(): JSX.Element {
   const [formData, setFormData] = useState<ReviewFormData>({
     review: '',
+    rating: 0,
   });
+
+  const isValid: boolean =
+    formData.review.length >= MIN_COMMENT_LENGTH &&
+    formData.review.length <= MAX_COMMENT_LENGTH &&
+    formData.rating !== 0;
 
   const handleCommentFieldChange = (evt: ChangeEvent<HTMLTextAreaElement>) => {
     const {name, value} = evt.target;
     setFormData({...formData, [name]: value});
   };
 
+  const handleRatingChange = (newRating: number) => {
+    setFormData({ ...formData, rating: newRating });
+  };
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        {RATINGS.map((rating) => <RatingInput key={rating.value} rating={rating}/>)}
+        {RATINGS.map((rating) => <RatingInput key={rating.value} onRatingChange={handleRatingChange} rating={rating}/>)}
       </div>
       <textarea
         onChange={handleCommentFieldChange}
@@ -30,14 +42,15 @@ function ReviewsForm(): JSX.Element {
         id="review"
         name="review"
         placeholder="Tell how was your stay, what you like and what can be improved"
-        minLength={MIN_COMMENT_SYMBOLS_COUNT}
+        minLength={MIN_COMMENT_LENGTH}
+        maxLength={MAX_COMMENT_LENGTH}
       >
       </textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={!isValid}>Submit</button>
       </div>
     </form>
   );
