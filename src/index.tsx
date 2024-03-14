c56cd41e46ef463a7a5d578cd64b8feeb5f49627
Quistis <suicidexhelia@gmail.com>
2024-03-14 21:09:05 +0500

    Убрал из компонента index лишние импорты

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
-import { Setting } from './const';
-import { offers } from './mock/offers';
 import { store } from './store';
 
 const root = ReactDOM.createRoot(
