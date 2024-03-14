5a02ab85d42ee963803c8af6417b68ad966ab3fa
Quistis <suicidexhelia@gmail.com>
2024-03-14 18:55:35 +0500

    Отрефакторил компонент LocationsList, вынес компонент LocationsListItem в отдельный файл, реализовал переключение активного города и его запись в store

---
+++ src/components/locations-list-item/locations-list-item.tsx
@@ -0,0 +1,42 @@
+import { MouseEvent } from 'react';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import { changeCity } from '../../store/action';
+import { CitiesType } from '../../const';
+
+type LocationsListItemProps = {
+  city: CitiesType;
+}
+
+function LocationsListItem({city}: LocationsListItemProps): JSX.Element {
+  const {name} = city;
+  const activeItemClass = 'tabs__item--active';
+  const currentCity = useAppSelector((state) => state.city);
+  const dispatch = useAppDispatch();
+
+  function handleCityClick(evt: MouseEvent<HTMLElement>) {
+    const target = evt.target as HTMLElement;
+    const targetCity = target.textContent;
+
+    if (targetCity) {
+      dispatch(changeCity({cityName: targetCity}));
+    }
+  }
+
+  return (
+    <li
+      className="locations__item"
+      onClick={handleCityClick}
+    >
+      <a
+        className={`locations__item-link tabs__item ${name === currentCity ? activeItemClass : ''}`}
+        href="#"
+      >
+        <span>
+          {name}
+        </span>
+      </a>
+    </li>
+  );
+}
+
+export default LocationsListItem;
