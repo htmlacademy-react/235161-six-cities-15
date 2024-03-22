98431a817f03b0fcd2c7a2855c222b210acc00b8
Quistis <suicidexhelia@gmail.com>
2024-03-22 17:19:21 +0500

    Отрефакторил offersSlice

--- src/store/slices/offersSlice.ts
+++ src/store/slices/offersSlice.ts
@@ -1,19 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getOffers } from '../action';
+// import { getOffers } from '../action';
 import { OfferType } from '../../types/offer';
 import { offers } from '../../mock/offers';
 
 const initialState: OfferType[] = [];
 
-//TODO: Нужно доработать
+//TODO: Нужно доработать, доработаю в 7 модуле
 const offersSlice = createSlice({
   name: 'offers',
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getOffers, () => [...offers]);
-  }
+  reducers: {
+    getOffers: () => [...offers]
+  },
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(getOffers, () => [...offers]);
+  // }
 });
 
 export const offersReducer = offersSlice.reducer;
