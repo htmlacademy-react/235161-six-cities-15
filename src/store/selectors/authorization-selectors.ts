f70e82204a7447c98aa0e6593cc4c56db83b0bfe
Quistis <suicidexhelia@gmail.com>
2024-04-09 18:38:59 +0500

    Удалил неиспользуемый селектор из authorization-selectors

--- src/store/selectors/authorization-selectors.ts
+++ src/store/selectors/authorization-selectors.ts
@@ -2,5 +2,4 @@ import { State } from '../../types/state';
 import { AuthorizationStatus, NameSpace } from '../../const';
 
 export const getAuthStatus = (state: State): AuthorizationStatus => state[NameSpace.Authorization].authStatus;
-export const getAuthErrorStatus = (state: State): boolean => state[NameSpace.Authorization].authErrorStatus;
 export const getAuthLoadingStatus = (state: State): boolean => state[NameSpace.Authorization].authLoadingStatus;
